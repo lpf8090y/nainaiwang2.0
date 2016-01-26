b2f1b402eaec86d39d172fbf657931fa5845cd22
ducong123 <dcwode123@163.com>
2016-01-26 09:07:00 +0800

    个人中心
    新增个人中心页面

--- js/user_index.js
+++ js/user_index.js
@@ -3,6 +3,9 @@ $(function(){
 		$(".user_zbrz").css("display","block");
 		$(".check-approve").css("display","none");
 		
-		
+	})
+	/*资金明细处，点击下来按钮选择操作*/
+	$(".dian").click(function(){
+		$(".cz_show").toggle();
 	})
 })
