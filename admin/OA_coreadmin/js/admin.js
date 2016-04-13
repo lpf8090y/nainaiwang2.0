c1c29e4da9ae7cc4cb68cdd9b9355063a88d2271
ducong123 <dcwode123@163.com>
2016-04-13 08:27:58 +0800

    增加admin.js

---
+++ admin/OA_coreadmin/js/admin.js
@@ -0,0 +1,7 @@
+$('#demo').change(function(){
+    var con = $(this).html();
+    
+     $("ul").append("<li  class="qxm"><input type="text" class="text" value="con"/></li>
+                    <li class="cz"><a><img src="css/img/icon_x.jpg"/></a></li>");
+
+})
