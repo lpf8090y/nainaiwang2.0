17aea08998cad41fbcfdbe63db83222ba796c77c
ducong123 <dcwode123@163.com>
2016-04-13 10:30:13 +0800

    修改添加js

--- admin/OA_coreadmin/js/admin.js
+++ admin/OA_coreadmin/js/admin.js
@@ -4,10 +4,12 @@
     $("#btn").click(function(){
 
     var item=$("#test").find("option:selected").text();
-     $("#app").append("<ul><li class='qxm'><input type='text' class='text' value="+item+"></li> <li class='cz'><a class='del'><img src='css/img/icon_x.jpg'/></a></li></ul>");
-      
-     /*$(".del").click(function(){
-            $(this).parent("ul").find("li").remove();
-        });*/
+     $("#app").append("<ul><li class='qxm'><input type='text' class='text' value="+item+"></li> <li class='cz'><a  class='del'><img  src='css/img/icon_x.jpg'/></a></li></ul>");
+
+       $(".del").click(function(){
+     	alert("d")
+            $(this).parent("li").parent("ul").remove();
+        });
+
     });
 });
