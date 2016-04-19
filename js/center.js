051dda25a6cfa63ef58a75f9d281cfb2b391f2cd
ducong123 <dcwode123@163.com>
2016-04-19 16:51:25 +0800

    个人中心修改
    个人中心修改其列表内容，给个报盘审核页面和采购审核页面增加弹框提示信息

--- js/center.js
+++ js/center.js
@@ -68,4 +68,26 @@ $(document).ready(function(){
   });
 });
 //end企业认证切换
+//发布报盘弹出提示窗
+jQuery(document).ready(function($){
+  //open popup
+  $('.bzjin').on('click', function(event){
+    event.preventDefault();
+    $('.cd-popup').addClass('is-visible');
+  });
+  
+  //close popup
+  $('.cd-popup').on('click', function(event){
+    if( $(event.target).is('.cd-popup-close') || $(event.target).is('.cd-popup') ) {
+      event.preventDefault();
+      $(this).removeClass('is-visible');
+    }
+  });
+  //close popup when clicking the esc keyboard button
+  $(document).keyup(function(event){
+      if(event.which=='27'){
+        $('.cd-popup').removeClass('is-visible');
+      }
+    });
+});
 
