d0f6e07d04123d97eb034600562b97c54772a95d
ducong123 <dcwode123@163.com>
2016-05-24 16:52:00 +0800

    添加分类页和忘记密码页面

--- moblies/demo/js/message.js
+++ moblies/demo/js/message.js
@@ -69,3 +69,13 @@
          })
 
     })
+//是否选中消息
+$(function() {
+$(".radio").click(function(){
+      $(".radio").removeClass("der_col");
+      $(".radio .radio_click").removeClass("radio_check");
+      $(this).addClass("der_col");
+      $(this).find(".radio_click").addClass("radio_check");
+    });
+});
+/*是否选中消息 end*/
