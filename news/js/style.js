ae1bc152dbdfc1b338a387b77757f3fa9a3f2d2f
ducong123 <dcwode123@163.com>
2017-02-06 16:04:05 +0800

    分页js

--- news/js/style.js
+++ news/js/style.js
@@ -9,6 +9,20 @@ $(function(){
 		$(".page .page_num").removeClass("on");
 		$(this).addClass('on');
 	})
+	//上一页
+	$(".page .page_up").click(function(){
+		var index = $(".page .page_num.on").index();//获取当前页
+			if(index > 2){
+				$(".page .page_num").removeClass("on");//清除所有选中
+				$(".page a").eq(index-1).addClass("on");//选中上一页
+
+			}
+	})
+	//点击首页效果
+	$(".page .page_first").click(function(){
+		$(".page .page_num").removeClass("on");
+		$(".page .page_num").eq(0).addClass("on")
+	})
 	//行业移入移除效果
 	$("ul li.quotation_li").mouseover(function () {
                 $(".quotation_ul_bage").show();
